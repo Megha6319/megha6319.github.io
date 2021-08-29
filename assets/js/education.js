4b4761a6ceabbcb65fc4e130d79c2c291787bc39
Megha6319 <59844589+Megha6319@users.noreply.github.com>
2021-08-30 02:19:32 +0530

    Update education.js

--- assets/js/education.js
+++ assets/js/education.js
@@ -6,7 +6,7 @@ const moocs = document.querySelector(".moocs");
 const moocscards = [
   {
     title: "Remote Sensing & GIS for Environmental Studies",
-    cardImage: "https://www.logotaglines.com/wp-content/uploads/2021/06/ISRO-Indian_Space_Research_Organisation-Logo-Slogan-tagline-vision-mission-motto-720x480.png",
+    cardImage: "https://karthik3685.files.wordpress.com/2008/03/isro-logo.jpg",
     moocLink: "https://www.linkedin.com/posts/activity-6828956260914868224-p61m/",
   },
   {
